ef36c4cc880158c1db85304b867166efed203214
Andrei <andrei.popabd@gmail.com>
2020-02-24 09:22:45 +0000

    FIXes the comments in searchObject

--- searchObject.php
+++ searchObject.php
@@ -36,12 +36,14 @@ class searchObject
 	/*
 	The function will output "NOTFOUND" if the searched item is not in that specific tree.
 	The function will output the path to that specific searched item if it is found in that tree.
-	The basecase: when we have no more subParents and we have not found the file.
-	The idea: the function returns the full all below levels(including children) if it is called on foundit =1
-	
-	if children are 0 and we have not found, we pop the first el of the stack
-	when we pop and lvl is 3( sizeof(subParents)+1) we add that name of the dir in the output and
-	then we append the recursiveSearch for each of its subParents
+	I will use the Breadth-first search algorithm to traverse the tree with a custom adaptation. 
+	Whenever I see a possible match for the word there are 2 cases:
+	If that node is a NodeChild, I print the path from the root to that node.
+	If that node is a NodeParent, I print the path from the root to that node.Now I have 3 cases:
+	The node NodeParent has no other NodeParent nodes, in this case I iterate through the children of that node, creating a path for each one of them.
+	The node NodeParent has no NodeChild nodes and it has only NodeParents Nodes, in this case I will print the whole subtree.
+	The node NodeParent has NodeParent nodes and NodeChildren nodes in this case I print all the path of NodeChildren and then I iterate through all the NodeParent nodes and I continue the algorithm. 
+
 	*/
 	
 	function recursiveSearch($parent,$searchedItem){
